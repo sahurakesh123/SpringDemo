94794dd176805eb249ae6d6f3bfadf30230add7d
sahurakesh123 <sahurakesh123@users.noreply.github.com>
2018-08-14 15:27:58 +0530

    SpringDemoBeanApplicationContextAware commit
    A good example of SpringDemoBeanApplicationContextAware.

---
+++ SpringDemoBeanApplicationContextAware/src/main/java/org/rakesh/sahu/elm/Triangle.java
@@ -0,0 +1,64 @@
+package org.rakesh.sahu.elm;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	private ApplicationContext context =null;
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	public void draw() {
+		System.out.println("Point A= " + getPointA().getX() + ", " + getPointA().getY());
+		System.out.println("Point B= " + getPointB().getX() + ", " + getPointB().getY());
+		System.out.println("Point C= " + getPointC().getX() + ", " + getPointC().getY());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.springframework.context.ApplicationContextAware#setApplicationContext(org
+	 * .springframework.context.ApplicationContext)
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang.String)
+	 */
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("Bean name ="+name);
+	}
+
+}
